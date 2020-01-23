69e48707e5f49558773bee82cd3d8d0be640e9cb
alu0100785050 <alu0100785050@ull.edu.es>
2020-01-23 12:57:21 +0000

    clase no necesaria

--- tfgmiriam/evento.py
+++
@@ -1,7 +0,0 @@
-class Evento:
-
-    def __init__(self, tipo_, comienzo_, final_, info_):
-        self.tipo = tipo_
-        self.comienzo = comienzo_
-        self.final = final_
-        self.info = info_
